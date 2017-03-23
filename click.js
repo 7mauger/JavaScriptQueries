7eec1f1f89f62b0330b7f4d208f864575ac0664e
7mauger <7mauger@stpatrick.org>
2017-03-23 10:49:49 -0500

    Add files via upload

---
+++ click.js
@@ -0,0 +1,19 @@
+//declare samecolor() here
+function samecolor(){
+  let newcolor = "#000000";
+  let divs = document.querySelectorAll("#samecolor div");
+
+  for(i=0;i<divs.length;i++){
+    document.divs[i].style.backgroundColor = newcolor;
+    document.divs[i].style.height = "500px"
+    document.divs[i].style.opacity = Math.random();
+  }
+}
+//declare diffcolor() here
+function diffcolor(){
+  let color = #990000;
+  let complement = #000099;
+  for(x=0;x<document.getElementsById("diffcolor");x++){
+    
+  }
+}
