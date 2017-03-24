a6b599ffbae289ce778c77427489fe2e5bd54aa4
7mauger <7mauger@stpatrick.org>
2017-03-24 10:40:04 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -2,18 +2,22 @@
 function samecolor(){
   let newcolor = "#000000";
   let divs = document.querySelectorAll("#samecolor div");
-
-  for(i=0;i<divs.length;i++){
-    document.divs[i].style.backgroundColor = newcolor;
-    document.divs[i].style.height = "500px"
-    document.divs[i].style.opacity = Math.random();
+  for(let i=0;i<divs.length;i++){
+    divs[i].style.backgroundColor = newcolor;
+    divs[i].style.opacity = Math.random();
   }
 }
 //declare diffcolor() here
 function diffcolor(){
-  let color = #990000;
-  let complement = #000099;
-  for(x=0;x<document.getElementsById("diffcolor");x++){
-    
+  let color = "#990000";
+  let complement = "#000099";
+  let divs2 = document.querySelectorAll("#diffcolor div");
+  for(let x=0;x<divs2.length;x++){
+    if(x==0 || x==2 || x==4 || x==6 || x==8){
+      divs2[x].style.backgroundColor = "#008732"
+    }
+    else{
+      divs2[x].style.backgroundColor = "#FF78CD"
+    }
   }
 }
